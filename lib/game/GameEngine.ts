85064cd6aaccd5fc248c4760e350141fc2cc41bf
jansee <jansen.alvesr@gmail.com>
2025-10-28 14:32:41 -0300

    feat: add GameEngine class with the game commands

---
+++ lib/game/GameEngine.ts
@@ -0,0 +1,56 @@
+import { Player } from "./Player";
+import Board from "./Board";
+import { STARTING_MONEY } from "../constants";
+import { GameState, MoveResult } from "../types";
+
+export class GameEngine {
+  players: Player[];
+  board: Board;
+  currentPlayerIndex: number;
+  round: number;
+
+  constructor(playerNames: string[] = []) {
+    this.players = playerNames.map((n, i) => new Player(String(i + 1), n, STARTING_MONEY));
+    this.board = new Board();
+    this.currentPlayerIndex = 0;
+    this.round = 1;
+  }
+
+  getGameState(): GameState {
+    return {
+      players: this.players.map((p) => ({ ...p })),
+      currentPlayerIndex: this.currentPlayerIndex,
+      round: this.round,
+    };
+  }
+
+  rollDice(): number {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  getCurrentPlayer(): Player | null {
+    return this.players[this.currentPlayerIndex] ?? null;
+  }
+
+  nextTurn(): void {
+    this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
+    if (this.currentPlayerIndex === 0) this.round += 1;
+  }
+
+  moveCurrentPlayer(steps: number): MoveResult | null {
+    const player = this.getCurrentPlayer();
+    if (!player) return null;
+    const from = player.position;
+    player.move(steps, this.board.tiles.length);
+    const to = player.position;
+    const tile = this.board.getTile(to);
+    return {
+      playerId: player.id,
+      from,
+      to,
+      tile,
+    };
+  }
+}
+
+export default GameEngine;
